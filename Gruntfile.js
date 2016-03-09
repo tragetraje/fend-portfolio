4dfcb6a9f600effdc3c3e063101c7ef00fd1705d
tragetraje <z.a.astafyeva@gmail.com>
2016-03-10 10:41:32 +1100

    initial upload

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            name: 'small',
+            width: 400,
+            quality: 30
+          },{
+            name: 'medium',
+            width: 800,
+            quality: 30
+          },{
+            name: 'large',
+            width: 1200,
+            quality: 40
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images/',
+          dest: 'images/'
+        }]
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.registerTask('default', ['responsive_images']);
+
+};
